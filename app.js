a10b98c47a343b7ce48070ed68a72b2d3ca619cf
Anushtha <anushthaniranjan@gmail.com>
2021-07-12 00:31:09 +0530

    Added single & multiple file feature

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const express = require('express');
+var multer = require('multer');
+var upload = multer({ storage: storage })
 
 const app = express();
 
 const port = 7000;
 
 app.use(express.json());
+//for singe file
 
-//ROUTES WILL GO HERE
-app.get('/', function(req, res) {
-    res.json({ message: 'WELCOME' });
+app.post('/single', upload.single('profile'), (req, res) => {
+    try {
+        res.send(req.file);
+    } catch (err) {
+        res.send(400);
+    }
+});
+
+//for multiple files
+app.post('/bulk', upload.array('profiles', 4), (req, res) => {
+    try {
+        res.send(req.files);
+    } catch (error) {
+        console.log(error);
+        res.send(400);
+    }
+});
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './upload');
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
 });
 
 app.listen(port, () => {
